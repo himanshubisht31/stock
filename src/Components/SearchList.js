a2d799414d88b609e2de2153c3e2c0aa142c9ec8
Himanshu <himanshubisht3112@gmail.com>
2021-12-20 11:49:52 +0530

    style

--- src/Components/SearchList.js
+++ src/Components/SearchList.js
@@ -2,9 +2,12 @@ import React, { useContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Buttons from "./Buttons";
 import { wishListContext } from "../Context/WishListContext";
+import "../Styles/Company.css";
 
 export default function SearchList({ stock }) {
   const [showButtons, setShowButtons] = useState(false);
+  const diff = +stock[2] - +stock[1];
+  const percent = parseFloat((diff * 100) / +stock[1]).toFixed(2);
   const [found, setFound] = useState(false);
   const { wish } = useContext(wishListContext);
 
@@ -28,12 +31,23 @@ export default function SearchList({ stock }) {
       }}
     >
       <div>
-        <div>{stock[0].split("::")[0]}</div>
-        <div>{stock[0].split("::")[1]}</div>
+        <div className={diff >= 0 ? "increaseStock" : "decreaseStock"}>
+          {stock[0].split("::")[0]}
+        </div>
+        <div>NSE</div>
       </div>
       <div>
-        <div>{stock[1]}</div>
-        <div>{stock[2]}</div>
+        <div className={diff >= 0 ? "increaseStock" : "decreaseStock"}>
+          {+stock[2]}
+        </div>
+        <div className="percentage">
+          {diff >= 0 ? (
+            <img src="/images/up.png" alt="" />
+          ) : (
+            <img src="/images/down.png" alt="" />
+          )}{" "}
+          <span>{`${percent}%`}</span>
+        </div>
       </div>
       {showButtons && <Buttons stock={stock} found={found} />}
     </div>
